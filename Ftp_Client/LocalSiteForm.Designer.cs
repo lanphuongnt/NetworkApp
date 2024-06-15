6aa92a48547160d0ce7b9d0da10eba6d30bb5497
lanphuongnt <phuongntl.work@gmail.com>
2024-06-15 13:47:32 +0700

    all code

---
+++ Ftp_Client/LocalSiteForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Ftp_Client
+{
+    partial class LocalSiteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            mainContainer = new SplitContainer();
+            folderTreeView = new TreeView();
+            fileBrowser = new WebBrowser();
+            localSiteLabel = new Label();
+            localPathTextBox = new TextBox();
+            backButton = new Button();
+            localFileMenuStrip = new ContextMenuStrip(components);
+            uploadFileToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)mainContainer).BeginInit();
+            mainContainer.Panel1.SuspendLayout();
+            mainContainer.Panel2.SuspendLayout();
+            mainContainer.SuspendLayout();
+            localFileMenuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // mainContainer
+            // 
+            mainContainer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            mainContainer.BorderStyle = BorderStyle.FixedSingle;
+            mainContainer.Location = new Point(0, 53);
+            mainContainer.Margin = new Padding(2);
+            mainContainer.Name = "mainContainer";
+            mainContainer.Orientation = Orientation.Horizontal;
+            // 
+            // mainContainer.Panel1
+            // 
+            mainContainer.Panel1.Controls.Add(folderTreeView);
+            // 
+            // mainContainer.Panel2
+            // 
+            mainContainer.Panel2.Controls.Add(fileBrowser);
+            mainContainer.Size = new Size(552, 520);
+            mainContainer.SplitterDistance = 202;
+            mainContainer.SplitterWidth = 1;
+            mainContainer.TabIndex = 1;
+            // 
+            // folderTreeView
+            // 
+            folderTreeView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            folderTreeView.Location = new Point(2, 2);
+            folderTreeView.Margin = new Padding(2);
+            folderTreeView.Name = "folderTreeView";
+            folderTreeView.Size = new Size(550, 196);
+            folderTreeView.TabIndex = 0;
+            folderTreeView.NodeMouseDoubleClick += folderTreeView_NodeMouseDoubleClick;
+            folderTreeView.MouseUp += FolderTreeView_MouseUp;
+            //folderTreeView.NodeMouseClick += FolderTreeView_NodeMouseClick;
+            // 
+            // fileBrowser
+            // 
+            fileBrowser.Dock = DockStyle.Fill;
+            fileBrowser.Location = new Point(0, 0);
+            fileBrowser.Margin = new Padding(2);
+            fileBrowser.MinimumSize = new Size(13, 16);
+            fileBrowser.Name = "fileBrowser";
+            fileBrowser.Size = new Size(550, 315);
+            fileBrowser.TabIndex = 0;
+            fileBrowser.DocumentCompleted += fileBrowser_DocumentCompleted;
+            // 
+            // localSiteLabel
+            // 
+            localSiteLabel.AutoSize = true;
+            localSiteLabel.Location = new Point(8, 17);
+            localSiteLabel.Margin = new Padding(2, 0, 2, 0);
+            localSiteLabel.Name = "localSiteLabel";
+            localSiteLabel.Size = new Size(74, 20);
+            localSiteLabel.TabIndex = 2;
+            localSiteLabel.Text = "Local site:";
+            // 
+            // localPathTextBox
+            // 
+            localPathTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            localPathTextBox.BorderStyle = BorderStyle.FixedSingle;
+            localPathTextBox.Location = new Point(85, 12);
+            localPathTextBox.Margin = new Padding(2);
+            localPathTextBox.Name = "localPathTextBox";
+            localPathTextBox.Size = new Size(350, 27);
+            localPathTextBox.TabIndex = 3;
+            localPathTextBox.TextChanged += localPathTextBox_TextChanged;
+            localPathTextBox.Leave += localPathTextBox_Leave;
+            localPathTextBox.PreviewKeyDown += localPathTextBox_PreviewKeyDown;
+            // 
+            // backButton
+            // 
+            backButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            backButton.Location = new Point(446, 4);
+            backButton.Margin = new Padding(2);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(98, 46);
+            backButton.TabIndex = 4;
+            backButton.Text = "Back";
+            backButton.UseVisualStyleBackColor = true;
+            backButton.Click += backButton_Click;
+            // 
+            // localFileMenuStrip
+            // 
+            localFileMenuStrip.ImageScalingSize = new Size(20, 20);
+            localFileMenuStrip.Items.AddRange(new ToolStripItem[] { uploadFileToolStripMenuItem });
+            localFileMenuStrip.Name = "localFileMenuStrip";
+            localFileMenuStrip.Size = new Size(211, 56);
+            // 
+            // uploadFileToolStripMenuItem
+            // 
+            uploadFileToolStripMenuItem.Name = "uploadFileToolStripMenuItem";
+            uploadFileToolStripMenuItem.Size = new Size(210, 24);
+            uploadFileToolStripMenuItem.Text = "Upload file";
+            uploadFileToolStripMenuItem.Click += UploadMenuItem_Click;
+            // 
+            // LocalSiteForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(552, 576);
+            Controls.Add(backButton);
+            Controls.Add(localPathTextBox);
+            Controls.Add(localSiteLabel);
+            Controls.Add(mainContainer);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
+            Name = "LocalSiteForm";
+            Text = "LocalSiteForm";
+            Load += LocalSiteForm_Load;
+            mainContainer.Panel1.ResumeLayout(false);
+            mainContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)mainContainer).EndInit();
+            mainContainer.ResumeLayout(false);
+            localFileMenuStrip.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer mainContainer;
+        private System.Windows.Forms.Label localSiteLabel;
+        private System.Windows.Forms.TextBox localPathTextBox;
+        private System.Windows.Forms.WebBrowser fileBrowser;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TreeView folderTreeView;
+        private ContextMenuStrip localFileMenuStrip;
+        private ToolStripMenuItem uploadFileToolStripMenuItem;
+    }
+}
---
+++ Ftp_Client/RemoteSiteForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Ftp_Client
+{
+    partial class RemoteSiteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            TreeNode treeNode1 = new TreeNode("\\");
+            remotePathTextBox = new TextBox();
+            remoteSiteLabel = new Label();
+            headerPanel = new Panel();
+            folderTreeRemote = new TreeView();
+            panel2 = new Panel();
+            fileListView = new ListView();
+            panel1 = new Panel();
+            fileMenuStrip = new ContextMenuStrip(components);
+            deleteFileToolStripMenuItem = new ToolStripMenuItem();
+            downloadFileToolStripMenuItem = new ToolStripMenuItem();
+            folderMenuStrip = new ContextMenuStrip(components);
+            deleteFolderToolStripMenuItem = new ToolStripMenuItem();
+            blankMenuStrip = new ContextMenuStrip(components);
+            newFolderToolStripMenuItem = new ToolStripMenuItem();
+            headerPanel.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            fileMenuStrip.SuspendLayout();
+            folderMenuStrip.SuspendLayout();
+            blankMenuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // remotePathTextBox
+            // 
+            remotePathTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            remotePathTextBox.BorderStyle = BorderStyle.FixedSingle;
+            remotePathTextBox.Location = new Point(169, 22);
+            remotePathTextBox.Name = "remotePathTextBox";
+            remotePathTextBox.Size = new Size(709, 39);
+            remotePathTextBox.TabIndex = 5;
+            remotePathTextBox.PreviewKeyDown += remotePathTextBox_PreviewKeyDown;
+            // 
+            // remoteSiteLabel
+            // 
+            remoteSiteLabel.AutoSize = true;
+            remoteSiteLabel.Location = new Point(18, 24);
+            remoteSiteLabel.Name = "remoteSiteLabel";
+            remoteSiteLabel.Size = new Size(145, 32);
+            remoteSiteLabel.TabIndex = 4;
+            remoteSiteLabel.Text = "Remote site:";
+            // 
+            // headerPanel
+            // 
+            headerPanel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            headerPanel.Controls.Add(remotePathTextBox);
+            headerPanel.Controls.Add(remoteSiteLabel);
+            headerPanel.Location = new Point(-2, 0);
+            headerPanel.Name = "headerPanel";
+            headerPanel.Size = new Size(897, 83);
+            headerPanel.TabIndex = 6;
+            // 
+            // folderTreeRemote
+            // 
+            folderTreeRemote.Dock = DockStyle.Fill;
+            folderTreeRemote.Location = new Point(0, 0);
+            folderTreeRemote.Name = "folderTreeRemote";
+            treeNode1.Name = "\\";
+            treeNode1.Text = "\\";
+            folderTreeRemote.Nodes.AddRange(new TreeNode[] { treeNode1 });
+            folderTreeRemote.Size = new Size(897, 378);
+            folderTreeRemote.TabIndex = 7;
+            folderTreeRemote.NodeMouseDoubleClick += folderTreeRemote_NodeMouseDoubleClick;
+            
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Controls.Add(fileListView);
+            panel2.Location = new Point(-2, 474);
+            panel2.Margin = new Padding(5);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(897, 475);
+            panel2.TabIndex = 10;
+            // 
+            // fileListView
+            // 
+            fileListView.Dock = DockStyle.Fill;
+            fileListView.Location = new Point(0, 0);
+            fileListView.Margin = new Padding(5);
+            fileListView.Name = "fileListView";
+            fileListView.Size = new Size(897, 475);
+            fileListView.TabIndex = 0;
+            fileListView.UseCompatibleStateImageBehavior = false;
+            fileListView.MouseUp += fileListView_MouseUp;
+            fileListView.MultiSelect = true;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(folderTreeRemote);
+            panel1.Location = new Point(-2, 91);
+            panel1.Margin = new Padding(5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(897, 378);
+            panel1.TabIndex = 11;
+            // 
+            // fileMenuStrip
+            // 
+            fileMenuStrip.ImageScalingSize = new Size(20, 20);
+            fileMenuStrip.Items.AddRange(new ToolStripItem[] { deleteFileToolStripMenuItem, downloadFileToolStripMenuItem });
+            fileMenuStrip.Name = "fileMenuStrip";
+            fileMenuStrip.Size = new Size(237, 80);
+            // 
+            // deleteFileToolStripMenuItem
+            // 
+            deleteFileToolStripMenuItem.Name = "deleteFileToolStripMenuItem";
+            deleteFileToolStripMenuItem.Size = new Size(236, 38);
+            deleteFileToolStripMenuItem.Text = "Delete file";
+            deleteFileToolStripMenuItem.Click += deleteFileToolStripMenuItem_Click;
+            // 
+            // downloadFileToolStripMenuItem
+            // 
+            downloadFileToolStripMenuItem.Name = "downloadFileToolStripMenuItem";
+            downloadFileToolStripMenuItem.Size = new Size(236, 38);
+            downloadFileToolStripMenuItem.Text = "Download file";
+            downloadFileToolStripMenuItem.Click += async (sender, e) => await downloadFileToolStripMenuItem_Click(sender, e);
+
+        // 
+        // folderMenuStrip
+        // 
+        folderMenuStrip.ImageScalingSize = new Size(20, 20);
+            folderMenuStrip.Items.AddRange(new ToolStripItem[] { deleteFolderToolStripMenuItem });
+            folderMenuStrip.Name = "folderMenuStrip";
+            folderMenuStrip.Size = new Size(229, 42);
+            // 
+            // deleteFolderToolStripMenuItem
+            // 
+            deleteFolderToolStripMenuItem.Name = "deleteFolderToolStripMenuItem";
+            deleteFolderToolStripMenuItem.Size = new Size(228, 38);
+            deleteFolderToolStripMenuItem.Text = "Delete folder";
+            deleteFolderToolStripMenuItem.Click += deleteFolderToolStripMenuItem_Click;
+            // 
+            // blankMenuStrip
+            // 
+            blankMenuStrip.ImageScalingSize = new Size(20, 20);
+            blankMenuStrip.Items.AddRange(new ToolStripItem[] { newFolderToolStripMenuItem });
+            blankMenuStrip.Name = "blankMenuStrip";
+            blankMenuStrip.Size = new Size(207, 42);
+            // 
+            // newFolderToolStripMenuItem
+            // 
+            newFolderToolStripMenuItem.Name = "newFolderToolStripMenuItem";
+            newFolderToolStripMenuItem.Size = new Size(206, 38);
+            newFolderToolStripMenuItem.Text = "New folder";
+            newFolderToolStripMenuItem.Click += newFolderToolStripMenuItem_Click;
+            // 
+            // RemoteSiteForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(895, 954);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Controls.Add(headerPanel);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "RemoteSiteForm";
+            Text = "RemoteSiteForm";
+            headerPanel.ResumeLayout(false);
+            headerPanel.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            fileMenuStrip.ResumeLayout(false);
+            folderMenuStrip.ResumeLayout(false);
+            blankMenuStrip.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox remotePathTextBox;
+        private System.Windows.Forms.Label remoteSiteLabel;
+        private System.Windows.Forms.Panel headerPanel;
+        private TreeView folderTreeRemote;
+        private Panel panel2;
+        private Panel panel1;
+        private ListView fileListView;
+        private ContextMenuStrip fileMenuStrip;
+        private ToolStripMenuItem deleteFileToolStripMenuItem;
+        private ContextMenuStrip folderMenuStrip;
+        private ToolStripMenuItem deleteFolderToolStripMenuItem;
+        private ContextMenuStrip blankMenuStrip;
+        private ToolStripMenuItem newFolderToolStripMenuItem;
+        private ToolStripMenuItem downloadFileToolStripMenuItem;
+    }
+}
